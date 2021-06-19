d593d37c7bd9c390815526f9939c5be7d7e4b92a
gustavo <gustavopcassol@gmail.com>
2021-06-19 20:36:56 -0300

    refactor Team

--- src/main/java/br/com/univali/Team.java
+++ src/main/java/br/com/univali/Team.java
@@ -1,37 +1,59 @@
 package br.com.univali;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 class Team {
-    private final List<Player> players;
+  private final Map<String, Player> playersByNumber;
 
-    Team(Player... players) {
-        this.players = Arrays.asList(players);
-        checkTeamFormation();
+  private Team(Map<String, Player> playersByNumber) {
+    this.playersByNumber = playersByNumber;
+  }
+
+  static class Builder {
+    private final Map<String, Player> playersByNumber = new HashMap<>();
+
+    Builder player(String number, Player player) {
+      if (playersByNumber.containsKey(number)) {
+        throw new IllegalArgumentException("There can be no players with duplicate numbers");
+      }
+      playersByNumber.put(number, player);
+      return this;
+    }
+
+    Team build() {
+      checkTeamFormation();
+      return new Team(playersByNumber);
     }
 
     private void checkTeamFormation() {
-        int goalkeepers = 1;
-        int defenders = 2;
-        int forwards = 2;
-        if (!(countPlayersIn(Position.GOALKEEPER) == goalkeepers &&
-                countPlayersIn(Position.DEFENDER) == defenders &&
-                countPlayersIn(Position.FORWARD) == forwards
-        )) {
-            throw new IllegalArgumentException("There should be " + goalkeepers + " " + Position.GOALKEEPER + ", " +
-                    defenders + " " + Position.DEFENDER + " and " +
-                    forwards + " " + Position.FORWARD);
-        }
+      Map<Position, Long> positionQuantity = collectPositionQuantity();
+      int goalkeepers = 1;
+      int defenders = 2;
+      int forwards = 2;
+      if (!(positionQuantity.remove(Position.GOALKEEPER) == goalkeepers
+          && positionQuantity.remove(Position.DEFENDER) == defenders
+          && positionQuantity.remove(Position.FORWARD) == forwards)) {
+        throw new IllegalArgumentException(
+            "There should be "
+                + goalkeepers
+                + " "
+                + Position.GOALKEEPER
+                + ", "
+                + defenders
+                + " "
+                + Position.DEFENDER
+                + " and "
+                + forwards
+                + " "
+                + Position.FORWARD);
+      }
     }
 
-    private int countPlayersIn(Position position) {
-        int counter = 0;
-        for (Player player : players) {
-            if (player.getPosition() == position) {
-                counter++;
-            }
-        }
-        return counter;
+    private Map<Position, Long> collectPositionQuantity() {
+      return playersByNumber.values().stream()
+          .collect(Collectors.groupingBy(Player::getPosition, Collectors.counting()));
     }
+  }
 }
