7bd631233a2c3ed71b407dc73673b52ae2d0f560
gustavo <gustavopcassol@gmail.com>
2021-06-20 02:47:43 -0300

    run code style and reorder methods

--- src/main/java/br/com/univali/Attribute.java
+++ src/main/java/br/com/univali/Attribute.java
@@ -1,38 +1,38 @@
 package br.com.univali;
 
 class Attribute {
-    private static final int MINIMUM_SCORE = 0;
-    private static final int MAXIMUM_SCORE = 100;
-    private static final int MINIMUM_HEIGHT = 0;
-    private static final int MAXIMUM_HEIGHT = 210;
+  private static final int MINIMUM_SCORE = 0;
+  private static final int MAXIMUM_SCORE = 100;
+  private static final int MINIMUM_HEIGHT = 0;
+  private static final int MAXIMUM_HEIGHT = 210;
 
-    private final int score;
+  private final int score;
 
-    private Attribute(int score) {
-        this.score = score;
+  private Attribute(int score) {
+    this.score = score;
+  }
+
+  static Attribute fromScore(int score) {
+    if (score < MINIMUM_SCORE || score > MAXIMUM_SCORE) {
+      throw new IllegalArgumentException(
+          "Value needs to be at least " + MINIMUM_SCORE + " and at most " + MAXIMUM_SCORE);
+    }
+    return new Attribute(score);
+  }
+
+  static Attribute fromHeight(int height) {
+    if (height < MINIMUM_HEIGHT) {
+      throw new IllegalArgumentException("Value needs to be at least " + MINIMUM_HEIGHT);
     }
 
-    static Attribute fromScore(int score) {
-        if (score < MINIMUM_SCORE || score > MAXIMUM_SCORE) {
-            throw new IllegalArgumentException("Value needs to be at least " + MINIMUM_SCORE
-                    + " and at most " + MAXIMUM_SCORE);
-        }
-        return new Attribute(score);
+    if (height >= MAXIMUM_HEIGHT) {
+      return new Attribute(MAXIMUM_SCORE);
     }
+    float score = (float) height * MAXIMUM_SCORE / MAXIMUM_HEIGHT;
+    return new Attribute((int) Math.rint(score));
+  }
 
-    static Attribute fromHeight(int height) {
-        if (height < MINIMUM_HEIGHT) {
-            throw new IllegalArgumentException("Value needs to be at least " + MINIMUM_HEIGHT);
-        }
-
-        if (height >= MAXIMUM_HEIGHT) {
-            return new Attribute(MAXIMUM_SCORE);
-        }
-        float score = (float) height * MAXIMUM_SCORE / MAXIMUM_HEIGHT;
-        return new Attribute((int) Math.rint(score));
-    }
-
-    int getScore() {
-        return score;
-    }
+  int getScore() {
+    return score;
+  }
 }
--- src/test/java/br/com/univali/AttributeTest.java
+++ src/test/java/br/com/univali/AttributeTest.java
@@ -6,42 +6,42 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class AttributeTest {
-    @Test
-    void testScoreLowerThanMinimum() {
-        Exception exception = assertThrows(IllegalArgumentException.class, () ->
-                Attribute.fromScore(-1));
-        assertEquals("Value needs to be at least 0 and at most 100", exception.getMessage());
-    }
+  @Test
+  void testMaximumHeight() {
+    Attribute attribute = Attribute.fromHeight(210);
+    assertEquals(100, attribute.getScore());
+  }
 
-    @Test
-    void testScoreHigherThanMaximum() {
-        Exception exception = assertThrows(IllegalArgumentException.class, () ->
-                Attribute.fromScore(101));
-        assertEquals("Value needs to be at least 0 and at most 100", exception.getMessage());
-    }
+  @Test
+  void testHeightConversionRoundUp() {
+    Attribute attribute = Attribute.fromHeight(104);
+    assertEquals(50, attribute.getScore());
+  }
 
-    @Test
-    void testHeightLowerThanMinimum() {
-        Exception exception = assertThrows(IllegalArgumentException.class, () ->
-                Attribute.fromHeight(-1));
-        assertEquals("Value needs to be at least 0", exception.getMessage());
-    }
+  @Test
+  void testHeightConversionRoundDown() {
+    Attribute attribute = Attribute.fromHeight(106);
+    assertEquals(50, attribute.getScore());
+  }
 
-    @Test
-    void testMaximumHeight() {
-        Attribute attribute = Attribute.fromHeight(210);
-        assertEquals(100, attribute.getScore());
-    }
+  @Test
+  void testScoreLowerThanMinimum() {
+    Exception exception =
+        assertThrows(IllegalArgumentException.class, () -> Attribute.fromScore(-1));
+    assertEquals("Value needs to be at least 0 and at most 100", exception.getMessage());
+  }
 
-    @Test
-    void testHeightConversionRoundUp() {
-        Attribute attribute = Attribute.fromHeight(104);
-        assertEquals(50, attribute.getScore());
-    }
+  @Test
+  void testScoreHigherThanMaximum() {
+    Exception exception =
+        assertThrows(IllegalArgumentException.class, () -> Attribute.fromScore(101));
+    assertEquals("Value needs to be at least 0 and at most 100", exception.getMessage());
+  }
 
-    @Test
-    void testHeightConversionRoundDown() {
-        Attribute attribute = Attribute.fromHeight(106);
-        assertEquals(50, attribute.getScore());
-    }
+  @Test
+  void testHeightLowerThanMinimum() {
+    Exception exception =
+        assertThrows(IllegalArgumentException.class, () -> Attribute.fromHeight(-1));
+    assertEquals("Value needs to be at least 0", exception.getMessage());
+  }
 }
