58d4b3d8608096de2fd2fbed07c6af7e895e8095
gustavo <gustavopcassol@gmail.com>
2021-06-22 00:19:41 -0300

    rename test variables

--- src/test/java/br/com/univali/TeamTest.java
+++ src/test/java/br/com/univali/TeamTest.java
@@ -8,30 +8,30 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class TeamTest {
-  private final Player player1 =
+  private final Player dida =
       new Player(
           "Dida", 32, Position.GOALKEEPER, Attribute.fromScore(100), Attribute.fromHeight(196));
-  private final Player player2 =
+  private final Player cafu =
       new Player("Cafu", 36, Position.DEFENDER, Attribute.fromScore(0), Attribute.fromScore(50));
-  private final Player player3 =
+  private final Player cicinho =
       new Player(
           "Cicinho", 33, Position.DEFENDER, Attribute.fromScore(50), Attribute.fromScore(50));
-  private final Player player4 =
+  private final Player adriano =
       new Player("Adriano", 24, Position.FORWARD, Attribute.fromScore(41), Attribute.fromScore(74));
-  private final Player player5 =
+  private final Player ronaldo =
       new Player("Ronaldo", 29, Position.FORWARD, Attribute.fromScore(0), Attribute.fromScore(50));
 
   @Test
   void testAddPlayersWithDuplicateNumbers() {
-    Team.Builder teamBuilder = new Team.Builder("Classic Brazil").player("1", player1);
+    Team.Builder teamBuilder = new Team.Builder("Brazil").player("1", dida);
     Exception exception =
-        assertThrows(IllegalArgumentException.class, () -> teamBuilder.player("1", player2));
+        assertThrows(IllegalArgumentException.class, () -> teamBuilder.player("1", cafu));
     assertEquals("There cannot be players with duplicate numbers", exception.getMessage());
   }
 
   @Test
   void testBuildWithoutEnoughPositions() {
-    Team.Builder teamBuilder = new Team.Builder("Classic Brazil").player("1", player1);
+    Team.Builder teamBuilder = new Team.Builder("Brazil").player("1", dida);
     Exception exception = assertThrows(IllegalArgumentException.class, teamBuilder::build);
     assertEquals("There should be 1 GOALKEEPER, 2 DEFENDER and 2 FORWARD", exception.getMessage());
   }
@@ -39,10 +39,7 @@ class TeamTest {
   @Test
   void testBuildInvalidQuantityInPositions() {
     Team.Builder teamBuilder =
-        new Team.Builder("Classic Brazil")
-            .player("1", player1)
-            .player("2", player2)
-            .player("6", player4);
+        new Team.Builder("Brazil").player("1", dida).player("2", cafu).player("6", adriano);
     Exception exception = assertThrows(IllegalArgumentException.class, teamBuilder::build);
     assertEquals("There should be 1 GOALKEEPER, 2 DEFENDER and 2 FORWARD", exception.getMessage());
   }
@@ -54,12 +51,12 @@ class TeamTest {
     @BeforeEach
     void build() {
       team =
-          new Team.Builder("Classic Brazil")
-              .player("1", player1)
-              .player("2", player2)
-              .player("6", player3)
-              .player("7", player4)
-              .player("9", player5)
+          new Team.Builder("Brazil")
+              .player("1", dida)
+              .player("2", cafu)
+              .player("6", cicinho)
+              .player("7", adriano)
+              .player("9", ronaldo)
               .build();
     }
 
@@ -68,14 +65,14 @@ class TeamTest {
     @Test
     void testReplaceNonexistent() {
       Exception exception =
-          assertThrows(IllegalArgumentException.class, () -> team.replace("-1", "1", player1));
+          assertThrows(IllegalArgumentException.class, () -> team.replace("-1", "1", dida));
       assertEquals("No existing player with such number", exception.getMessage());
     }
 
     @Test
     void testReplaceInvalidNewPosition() {
       Exception exception =
-          assertThrows(IllegalArgumentException.class, () -> team.replace("1", "2", player2));
+          assertThrows(IllegalArgumentException.class, () -> team.replace("1", "2", cafu));
       assertEquals(
           "The position of the new player needs to be the same as the one being replaced",
           exception.getMessage());
@@ -84,7 +81,7 @@ class TeamTest {
     @Test
     void testReplaceWithExistingNumber() {
       Exception exception =
-          assertThrows(IllegalArgumentException.class, () -> team.replace("2", "6", player2));
+          assertThrows(IllegalArgumentException.class, () -> team.replace("2", "6", cafu));
       assertEquals("There cannot be players with duplicate numbers", exception.getMessage());
     }
   }
