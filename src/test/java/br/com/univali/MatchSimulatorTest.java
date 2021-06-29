7dcd103b3260e08f66023afd83762b36dceb2c8b
gustavo <gustavopcassol@gmail.com>
2021-06-28 22:44:46 -0300

    replace teams for mocks in MatchSimulatorTest

--- src/test/java/br/com/univali/MatchSimulatorTest.java
+++ src/test/java/br/com/univali/MatchSimulatorTest.java
@@ -1,6 +1,10 @@
 package br.com.univali;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.Clock;
 import java.time.Instant;
@@ -9,76 +13,18 @@ import java.time.ZoneId;
 import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class MatchSimulatorTest {
   private final LocalDate localDate = LocalDate.of(2006, 9, 3);
-  private final MatchSimulator matchSimulator = createMatchSimulator();
+  private MatchSimulator matchSimulator;
 
-  private MatchSimulator createMatchSimulator() {
-    return new MatchSimulator(buildBrazil(), buildArgentina(), new Random(0), createClock());
-  }
-
-  private Team buildBrazil() {
-    var dida =
-        new Player(
-            "Dida", 32, Position.GOALKEEPER, Attribute.fromScore(100), Attribute.fromHeight(196));
-    var cafu =
-        new Player("Cafu", 36, Position.DEFENDER, Attribute.fromScore(0), Attribute.fromScore(50));
-    var cicinho =
-        new Player(
-            "Cicinho", 33, Position.DEFENDER, Attribute.fromScore(50), Attribute.fromScore(50));
-    var adriano =
-        new Player(
-            "Adriano", 24, Position.FORWARD, Attribute.fromScore(41), Attribute.fromScore(74));
-    var ronaldo =
-        new Player(
-            "Ronaldo", 29, Position.FORWARD, Attribute.fromScore(0), Attribute.fromScore(50));
-
-    return new Team.Builder("Brazil")
-        .player("1", dida)
-        .player("2", cafu)
-        .player("6", cicinho)
-        .player("7", adriano)
-        .player("9", ronaldo)
-        .build();
-  }
-
-  private Team buildArgentina() {
-    var robertoAbbondanzieri =
-        new Player(
-            "Roberto Abbondanzieri",
-            33,
-            Position.GOALKEEPER,
-            Attribute.fromScore(0),
-            Attribute.fromHeight(196));
-    var robertoAyala =
-        new Player(
-            "Roberto Ayala", 33, Position.DEFENDER, Attribute.fromScore(0), Attribute.fromScore(0));
-    var fabricioColoccini =
-        new Player(
-            "Fabricio Coloccini",
-            24,
-            Position.DEFENDER,
-            Attribute.fromScore(50),
-            Attribute.fromScore(50));
-    var javierSaviola =
-        new Player(
-            "Javier Saviola",
-            24,
-            Position.FORWARD,
-            Attribute.fromScore(41),
-            Attribute.fromScore(74));
-    var lionelMessi =
-        new Player(
-            "Lionel Messi", 18, Position.FORWARD, Attribute.fromScore(90), Attribute.fromScore(90));
-
-    return new Team.Builder("Argentina")
-        .player("1", robertoAbbondanzieri)
-        .player("2", robertoAyala)
-        .player("4", fabricioColoccini)
-        .player("7", javierSaviola)
-        .player("19", lionelMessi)
-        .build();
+  @BeforeEach
+  void init(@Mock Team team1, @Mock Team team2) {
+    when(team1.calculateTotalSkill()).thenReturn(265);
+    when(team2.calculateTotalSkill()).thenReturn(231);
+    matchSimulator = new MatchSimulator(team1, team2, new Random(0), createClock());
   }
 
   private Clock createClock() {
