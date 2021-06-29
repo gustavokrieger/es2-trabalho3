9c075fd8818f50b21fd26510d19241cd6ffb622e
gustavo <gustavopcassol@gmail.com>
2021-06-29 00:53:55 -0300

    refactor PlayerTest

--- src/test/java/br/com/univali/PlayerTest.java
+++ src/test/java/br/com/univali/PlayerTest.java
@@ -1,13 +1,24 @@
 package br.com.univali;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class PlayerTest {
-  private final Player player =
-      new Player(
-          "Tsubasa", 16, Position.FORWARD, Attribute.fromScore(100), Attribute.fromScore(100));
+  @Mock private Attribute primary;
+  @Mock private Attribute secondary;
+  private Player player;
+
+  @BeforeEach
+  void init() {
+    player = new Player("Tsubasa", 16, Position.FORWARD, primary, secondary);
+  }
 
   @Test
   void testScoreGoal() {
@@ -17,6 +28,9 @@ class PlayerTest {
 
   @Test
   void testCalculateSkill() {
+    when(primary.getScore()).thenReturn(100);
+    when(secondary.getScore()).thenReturn(100);
+
     assertEquals(100, player.calculateSkill());
   }
 }
