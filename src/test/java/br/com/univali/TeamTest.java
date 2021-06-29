3d68810190766d12d6e6bea9548005f9fc6e98f5
gustavo <gustavopcassol@gmail.com>
2021-06-29 20:56:13 -0300

    refactor TeamTest

--- src/test/java/br/com/univali/TeamTest.java
+++ src/test/java/br/com/univali/TeamTest.java
@@ -3,60 +3,81 @@ package br.com.univali;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class TeamTest {
-  private final Player dida =
-      new Player(
-          "Dida", 32, Position.GOALKEEPER, Attribute.fromScore(100), Attribute.fromHeight(196));
-  private final Player cafu =
-      new Player("Cafu", 36, Position.DEFENDER, Attribute.fromScore(0), Attribute.fromScore(50));
-  private final Player cicinho =
-      new Player(
-          "Cicinho", 33, Position.DEFENDER, Attribute.fromScore(50), Attribute.fromScore(50));
-  private final Player adriano =
-      new Player("Adriano", 24, Position.FORWARD, Attribute.fromScore(41), Attribute.fromScore(74));
-  private final Player ronaldo =
-      new Player("Ronaldo", 29, Position.FORWARD, Attribute.fromScore(0), Attribute.fromScore(50));
+  @Nested
+  class BeforeBuild {
+    private Team.Builder teamBuilder;
 
-  @Test
-  void testAddPlayersWithDuplicateNumbers() {
-    Team.Builder teamBuilder = new Team.Builder("Brazil").player("1", dida);
-    Exception exception =
-        assertThrows(IllegalArgumentException.class, () -> teamBuilder.player("1", cafu));
-    assertEquals("There cannot be players with duplicate numbers", exception.getMessage());
-  }
+    @BeforeEach
+    void init() {
+      teamBuilder = new Team.Builder("Brazil");
+    }
 
-  @Test
-  void testBuildWithoutEnoughPositions() {
-    Team.Builder teamBuilder = new Team.Builder("Brazil").player("1", dida);
-    Exception exception = assertThrows(IllegalArgumentException.class, teamBuilder::build);
-    assertEquals("There should be 1 GOALKEEPER, 2 DEFENDER and 2 FORWARD", exception.getMessage());
-  }
+    @Test
+    void testAddPlayersWithDuplicateNumbers(@Mock Player player1, @Mock Player player2) {
+      teamBuilder.player("1", player1);
+      Exception exception =
+          assertThrows(IllegalArgumentException.class, () -> teamBuilder.player("1", player2));
+      assertEquals("There cannot be players with duplicate numbers", exception.getMessage());
+    }
 
-  @Test
-  void testBuildInvalidQuantityInPositions() {
-    Team.Builder teamBuilder =
-        new Team.Builder("Brazil").player("1", dida).player("2", cafu).player("6", adriano);
-    Exception exception = assertThrows(IllegalArgumentException.class, teamBuilder::build);
-    assertEquals("There should be 1 GOALKEEPER, 2 DEFENDER and 2 FORWARD", exception.getMessage());
+    @Test
+    void testBuildWithoutEnoughPositions(@Mock Player player1) {
+      when(player1.getPosition()).thenReturn(Position.GOALKEEPER);
+
+      teamBuilder.player("1", player1);
+      Exception exception = assertThrows(IllegalArgumentException.class, teamBuilder::build);
+      assertEquals(
+          "There should be 1 GOALKEEPER, 2 DEFENDER and 2 FORWARD", exception.getMessage());
+    }
+
+    @Test
+    void testBuildInvalidQuantityInPositions(
+        @Mock Player player1, @Mock Player player2, @Mock Player player3) {
+      when(player1.getPosition()).thenReturn(Position.GOALKEEPER);
+      when(player2.getPosition()).thenReturn(Position.DEFENDER);
+      when(player3.getPosition()).thenReturn(Position.FORWARD);
+
+      teamBuilder.player("1", player1).player("2", player2).player("3", player3);
+      Exception exception = assertThrows(IllegalArgumentException.class, teamBuilder::build);
+      assertEquals(
+          "There should be 1 GOALKEEPER, 2 DEFENDER and 2 FORWARD", exception.getMessage());
+    }
   }
 
   @Nested
   class AfterBuild {
+    @Mock private Player player1;
+    @Mock private Player player2;
+    @Mock private Player player3;
+    @Mock private Player player4;
+    @Mock private Player player5;
     private Team team;
 
     @BeforeEach
-    void build() {
+    void init() {
+      when(player1.getPosition()).thenReturn(Position.GOALKEEPER);
+      when(player2.getPosition()).thenReturn(Position.DEFENDER);
+      when(player3.getPosition()).thenReturn(Position.DEFENDER);
+      when(player4.getPosition()).thenReturn(Position.FORWARD);
+      when(player5.getPosition()).thenReturn(Position.FORWARD);
+
       team =
           new Team.Builder("Brazil")
-              .player("1", dida)
-              .player("2", cafu)
-              .player("6", cicinho)
-              .player("7", adriano)
-              .player("9", ronaldo)
+              .player("1", player1)
+              .player("2", player2)
+              .player("3", player3)
+              .player("4", player4)
+              .player("5", player5)
               .build();
     }
 
@@ -65,35 +86,39 @@ class TeamTest {
     @Test
     void testReplaceNonexistent() {
       Exception exception =
-          assertThrows(IllegalArgumentException.class, () -> team.replace("-1", "1", dida));
+          assertThrows(IllegalArgumentException.class, () -> team.replace("0", "1", player1));
       assertEquals("No existing player with such number", exception.getMessage());
     }
 
     @Test
-    void testReplaceInvalidNewPosition() {
+    void testReplaceInvalidNewPosition(@Mock Player replacement) {
+      when(replacement.getPosition()).thenReturn(Position.DEFENDER);
+
       Exception exception =
-          assertThrows(IllegalArgumentException.class, () -> team.replace("1", "2", cafu));
+          assertThrows(IllegalArgumentException.class, () -> team.replace("1", "1", replacement));
       assertEquals(
           "The position of the new player needs to be the same as the one being replaced",
           exception.getMessage());
     }
 
     @Test
-    void testReplaceWithExistingNumber() {
+    void testReplaceWithExistingNumber(@Mock Player replacement) {
+      when(replacement.getPosition()).thenReturn(Position.GOALKEEPER);
+
       Exception exception =
-          assertThrows(IllegalArgumentException.class, () -> team.replace("2", "6", cafu));
+          assertThrows(IllegalArgumentException.class, () -> team.replace("1", "2", replacement));
       assertEquals("There cannot be players with duplicate numbers", exception.getMessage());
     }
 
     @Test
     void testCalculateTotalSkill() {
-      int totalSkill =
-          dida.calculateSkill()
-              + cafu.calculateSkill()
-              + cicinho.calculateSkill()
-              + adriano.calculateSkill()
-              + ronaldo.calculateSkill();
-      assertEquals(totalSkill, team.calculateTotalSkill());
+      when(player1.calculateSkill()).thenReturn(1);
+      when(player2.calculateSkill()).thenReturn(1);
+      when(player3.calculateSkill()).thenReturn(1);
+      when(player4.calculateSkill()).thenReturn(1);
+      when(player5.calculateSkill()).thenReturn(1);
+
+      assertEquals(5, team.calculateTotalSkill());
     }
   }
 }
